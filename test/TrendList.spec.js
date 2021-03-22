d95fd07d9d004362a8d5d972bc03e364b94c452e
miily8310s <rillakerika@gmail.com>
2021-03-22 22:16:31 +0900

    add render html test

--- test/TrendList.spec.js
+++ test/TrendList.spec.js
@@ -10,12 +10,34 @@ describe('TrendList', () => {
     })
     expect(wrapper.vm).toBeTruthy()
   })
-  test('is a Vue instance', () => {
+  test('fetch data is nothing', () => {
     const wrapper = mount(TrendList, {
       propsData: {
         trends: [],
       },
     })
-    expect(wrapper.vm).toBeTruthy()
+    expect(wrapper.html()).toBe('<div class="trendlist"></div>')
+  })
+  test('fetch data is having', () => {
+    const wrapper = mount(TrendList, {
+      propsData: {
+        trends: [
+          {
+            index: 1,
+            title: 'alaska',
+            media_type: 'movie',
+            poster_path: 'hoge.png',
+          },
+        ],
+      },
+    })
+    expect(wrapper.html()).toBe(
+      `<div class="trendlist">
+        <div>
+          <img src="https://themoviedb.org/t/p/w600_and_h900_bestv2/hoge.png" alt="1">
+          <p>alaska</p>
+        </div>
+      </div>`
+    )
   })
 })
